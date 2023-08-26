ff2195dd227fd83e7da7c31f0bd7daafc4de47b4
rakesh <126374141+rakeshjson@users.noreply.github.com>
2023-08-26 23:25:47 +0530

    Update script.js
    Added rotation function to rotate the Div

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ dragElement(document.getElementById("main-container"));
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    document.getElementById("main-container").onmousedown = dragMouseDown;
+    document.getElementById("heading").onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
         e.preventDefault();
-    
+
         pos3 = e.clientX;
         pos4 = e.clientY;
 
@@ -21,7 +21,7 @@ function dragElement(elmnt) {
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
         pos4 = e.clientY;
-     
+
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
@@ -30,4 +30,30 @@ function dragElement(elmnt) {
         document.onmouseup = null;
         document.onmousemove = null;
     }
+}
+
+function colorChanger(e) {
+    let color_id = e.target.id
+    document.getElementById('heading').style.backgroundColor = color_id
+
+}
+
+let cur_val = 0
+function rotate() {
+
+    if (cur_val == 1) {
+        document.querySelector('#main-container').style.transform = "rotate(90deg)"
+    }
+    else if (cur_val == 2) {
+        document.querySelector('#main-container').style.transform = "rotate(180deg)"
+    }
+    else if (cur_val == 3) {
+        document.querySelector('#main-container').style.transform = "rotate(270deg)"
+    }
+    else  {
+        document.querySelector('#main-container').style.transform = "rotate(360deg)"
+    }
+
+    if (cur_val == 4) { cur_val = 0 }
+    cur_val++
 }
